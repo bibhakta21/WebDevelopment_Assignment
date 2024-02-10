02aec9c5cb73d0fb9b23896d181e2bca399ec0ab
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-10 20:23:47 +0545

    User repository completed.

---
+++ Backend/src/main/java/com/example/decor/Repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.example.trek.Repository;
+
+import com.example.trek.Entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long>  {
+    boolean existsByUsernameOrEmail(String username, String email);
+
+    // Add additional query methods if needed
+    Optional<Users> findByUsernameOrEmail(String username,String email);
+//
+//    //forgot pass
+
+//    Optional<Users> findByEmail(String email);
+//    Optional<Users> findByResetToken(String resetToken);
+
+
+}
