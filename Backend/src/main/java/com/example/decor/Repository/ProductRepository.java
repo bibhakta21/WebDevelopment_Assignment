6ebcc896bbc19820178a144a50e9e85bc9b737a0
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-10 20:24:40 +0545

    repository for product completed.

---
+++ Backend/src/main/java/com/example/decor/Repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.trek.Repository;
+
+import com.example.trek.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
