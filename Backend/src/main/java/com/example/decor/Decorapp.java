51de5b18634b07aba3017d20b3356612daea1ffb
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-10 10:44:18 +0545

    Project Started

---
+++ Backend/src/main/java/com/example/decor/Decorapp.java
@@ -0,0 +1,13 @@
+package com.example.decor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Decorapp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Decorapp.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/example/decor/decornepalTests.java
@@ -0,0 +1,13 @@
+package com.example.decor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class decornepalTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
