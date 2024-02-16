8f659478e2bcc20590c0ea65a4a9bd3ebd19a748
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-16 21:49:15 +0545

    routing for homepage,navbar,footer completed

--- Frontend/decor web app/src/App.jsx
+++ Frontend/decor web app/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route,} from "react-router-dom";
 
 //All Homepage user and admin both
 import Home from "./components/Home";
+import Navigation from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 
