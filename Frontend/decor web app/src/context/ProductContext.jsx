1fd04f8b546777ed7f40517731cca4fdc9c2535e
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-18 10:47:32 +0545

    Fetch product api called successfully

--- Frontend/decor web app/src/context/ProductContext.jsx
+++ Frontend/decor web app/src/context/ProductContext.jsx
@@ -2,8 +2,22 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 const ProductContext = createContext();
-
+const [products, setProducts] = useState([]);
+const url = "http://localhost:8080/api/v2/products/getAll";
 export const ProductProvider = ({ children }) => {
+
+
+    const fetchProducts = async () => {
+        try {
+          const response = await axios.get(url);
+          setProducts(response.data);
+          setError(null); // Reset error on successful fetch
+        } catch (error) {
+          console.error("Error fetching data:", error);
+          setError("Error fetching productss."); // Set error message
+        }
+      };
+    
     return (
         <ProductContext.Provider
           value={{
