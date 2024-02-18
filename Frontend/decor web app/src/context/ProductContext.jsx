bf4169d5e1c0fcc85c34f1fb8acb62b375128165
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-18 10:45:36 +0545

    added context provider for other components

--- Frontend/decor web app/src/context/ProductContext.jsx
+++ Frontend/decor web app/src/context/ProductContext.jsx
@@ -5,7 +5,19 @@ const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
     return (
-      <></>
+        <ProductContext.Provider
+          value={{
+            products,
+            fetchProducts,
+            addProduct,
+            deleteProduct,
+            getProductById,
+            singleProduct,
+            error, // Add error to the context
+          }}
+        >
+          {children}
+        </ProductContext.Provider>
       );
     };
     export const useProductContext = () => {
