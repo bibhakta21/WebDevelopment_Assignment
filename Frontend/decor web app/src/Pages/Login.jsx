c9af6516323862b8588bf4c0816670bbf7951d7e
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-20 09:47:37 +0545

    signin custome hook imported

--- Frontend/decor web app/src/Pages/Login.jsx
+++ Frontend/decor web app/src/Pages/Login.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSignin } from "../context/useSignin";
 
 
 const Login = () => {
+    const { signin } = useSignin();
+ 
+  
   
 
   return (
