0e3e0abb1f7a0ccb0fa0626c5f5803a66797bd13
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-20 09:33:59 +0545

    necessary setup completed for registration

---
+++ Frontend/decor web app/src/Pages/Registration.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+ 
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Register;
