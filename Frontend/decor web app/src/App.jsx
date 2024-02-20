a95b15758f870aad7e9826a8b64e4697d50b6540
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-20 09:43:11 +0545

    if admin start with dashboard completed.

--- Frontend/decor web app/src/App.jsx
+++ Frontend/decor web app/src/App.jsx
@@ -1,15 +1,28 @@
-import { Routes, Route,} from "react-router-dom";
+import { Routes, Route,useLocation, useNavigate } from "react-router-dom";
 
 //All Homepage user and admin both
 import Home from "./components/Home";
 import Navigation from "./components/Navbar";
 import Footer from "./components/Footer";
+import Login from "./Pages/Login";
+import Register from "./Pages/Registration";
 
 
 
 
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // Determine whether to show the Navigation component based on the current route
+  const showNavigation = !location.pathname.startsWith("/dashboard");
+  const { user } = useAuthContext();
+
+  const redirect = () => {
+    // Dispatch the "LOGOUT" action to update the user state
+    navigate("/");
+  };
 
 
 
@@ -20,6 +33,8 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
        
       </Routes>
        <Footer />
