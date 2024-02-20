2f5fbfc5374164a69aa295bb4caa07b9ca119252
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-20 09:30:36 +0545

    necessary component imported

--- Frontend/decor web app/src/main.jsx
+++ Frontend/decor web app/src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { ProductProvider } from "./context/ProductContext.jsx";
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
