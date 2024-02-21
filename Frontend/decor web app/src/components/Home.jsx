03678988d74fbebcd3cffb72235930723b7fb190
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-21 10:04:42 +0545

    icons for showing different icon in home page setup done

--- Frontend/decor web app/src/components/Home.jsx
+++ Frontend/decor web app/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LandingPage from "./LandingPage";
+import ItemIcons from "./ItemIcons";
 
 
 const Home = () => {
   return (
     <>
       <LandingPage />
-      
+      <ItemIcons />
+    
     </>
   );
 };
---
+++ Frontend/decor web app/src/components/ItemIcons.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ItemIcons = () => {
+  return (
+    <section className="banner__1">
+    
+  </section>
+
+  );
+};
+
+export default ItemIcons;
