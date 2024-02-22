a2ccb1425aa676614318c495af532bd1f8c03848
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-22 20:09:01 +0545

    trying to add first service static data

--- Frontend/decor web app/src/components/Services.jsx
+++ Frontend/decor web app/src/components/Services.jsx
@@ -7,6 +7,13 @@ import {
 } from "react-icons/fa";
 
 const ServiceData = [
+  {
+    id: 1,
+    icon: <FaCarSide className="text-4xl md:text-5xl" style={{color:"red"}}/>,
+    title: "Free Shipping",
+    description: "Free Shipping On All Order",
+  },
+ 
   
 ];
 
