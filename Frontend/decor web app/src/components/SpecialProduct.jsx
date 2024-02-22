f7e71a9194ad0a4f1af7b9848b3d1a90384aa6bf
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-22 20:12:15 +0545

    function made

---
+++ Frontend/decor web app/src/components/SpecialProduct.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+
+const SpecialProduct = () => {
+
+
+  return (
+    <section className="product">
+     
+    </section>
+  );
+};
+
+export default SpecialProduct;
